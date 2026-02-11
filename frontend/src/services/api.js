2d579527edb4f358d8644da33ca2f4e0398878a9
Bhargavi Bodala <bhargavi202910@gmail.com>
2026-02-11 11:15:43 +0530

    Update backend URL to Render

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,26 @@
+// import axios from 'axios'
+
+// const api = axios.create({
+//   baseURL:  'http://localhost:5000/api',
+
+
+// })
+
+// api.interceptors.request.use((config) => {
+//   const token = localStorage.getItem('token')
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`
+//   }
+//   return config
+// })
+
+// export default api
+
 import axios from 'axios'
 
+// Replace with your Render backend URL
 const api = axios.create({
-  baseURL:  'http://localhost:5000/api',
-
-
+  baseURL: 'https://authdashboard.onrender.com/api',
 })
 
 api.interceptors.request.use((config) => {
@@ -15,4 +32,5 @@ api.interceptors.request.use((config) => {
 })
 
 export default api
+
 
